7cf4bcc6ebcfc457344c32a79c2216069bce76bd
Pedro Flores <pedro_flores_16@hotmail.com>
2024-09-23 15:28:47 +0100

    Only prompt once for login

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
@@ -7,14 +7,8 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
+      if (isLoggedIn) return true;
+      return false; // Redirect unauthenticated users to login page
     },
   },
   providers: [], // Add providers with an empty array for now
